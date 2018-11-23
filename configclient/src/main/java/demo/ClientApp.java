f8027c6d96c0c6e53d0387212ee6e51ea0051569
lij <tawalisa@163.com>
2018-11-23 11:36:37 +0800

    configuration server and config

---
+++ configclient/src/main/java/demo/ClientApp.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
+@RefreshScope
+public class ClientApp {
+    @Value("${bar:World!}")
+    String bar;
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello " + bar + "!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApp.class, args);
+    }
+}
